53360e93d913d001743c8622170c80b45af73b47
gabrigomezayuso <ggomezayuso@gmail.com>
2020-11-30 12:02:36 +0100

    mejoras

--- src/app/components/carta/carta.component.ts
+++ src/app/components/carta/carta.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carta.component.css']
 })
 export class CartaComponent implements OnInit {
+  vacio:string;
+  public contenido: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  elegirAccion(accion) {
+    //en caso de que la accion sea "anadir"
+    if(accion.target.value == "cat") {
+      //mostrara el contenido "anadir" y asi succesivamente
+      this.contenido = "cat"
+    } else if(accion.target.value == "cast") {
+      this.contenido = "cast"
+    } else if (accion.target.value == "eng") {
+      this.contenido = "eng"
+    }else if (accion.target.value == "fr") {
+      this.contenido = "fr"
+    } else {
+      //en caso de no ser ninguna de las 3, se mostrara un espacio vacio
+      this.vacio = "";
+    }
+  }
+
 
 }
